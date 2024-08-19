b5a7487e149288732b140084bbf448977de9447a
Pragyanshu Panda <prag2468@gmail.com>
2024-08-20 01:11:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, VideoProcessorBase
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 import av
 import cv2
 import numpy as np
@@ -8,7 +8,7 @@ from keras.preprocessing.image import img_to_array
 import mediapipe as mp
 
 # Load the face classifier and emotion detection model
-face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 classifier = load_model('modelv1.h5')
 classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
@@ -19,11 +19,16 @@ mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, min_detection_confidence=0.5)
 
 class EmotionDetector(VideoProcessorBase):
+    def __init__(self):
+        self.face_classifier = face_classifier
+        self.classifier = classifier
+        self.emotion_labels = emotion_labels
+        self.face_mesh = face_mesh
+
     def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
         img = frame.to_ndarray(format="bgr24")
-
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_classifier.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+        faces = self.face_classifier.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
@@ -35,14 +40,14 @@ class EmotionDetector(VideoProcessorBase):
                 roi = img_to_array(roi)
                 roi = np.expand_dims(roi, axis=0)
 
-                prediction = classifier.predict(roi)[0]
-                label = emotion_labels[prediction.argmax()]
+                prediction = self.classifier.predict(roi)[0]
+                label = self.emotion_labels[prediction.argmax()]
                 label_position = (x, y)
                 cv2.putText(img, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
                 # Convert the region of interest to RGB for Mediapipe processing
                 roi_rgb = cv2.cvtColor(img[y:y+h, x:x+w], cv2.COLOR_BGR2RGB)
-                results = face_mesh.process(roi_rgb)
+                results = self.face_mesh.process(roi_rgb)
 
                 # Draw the facial landmarks using Mediapipe
                 if results.multi_face_landmarks:
